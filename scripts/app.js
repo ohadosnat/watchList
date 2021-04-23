fd86c6c8a0ee727bd6f3fcc979ef77085381d2dd
Ohad <ohadosnatcv@gmail.com>
2021-04-23 10:03:36 +0300

    hover fixed

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,7 @@
 const section = document.querySelector('section');
-const boxContent = document.querySelectorAll('#boxContent')
-const watchToggle = document.querySelectorAll('#watchToggle')
+
 
 const watchlist = 'watchlist.json'
-const boxImg = boxContent.firstElementChild;
 
 const myList = new siteUI(section);
 const myWatchlist = new Watchlist(watchlist);
@@ -21,42 +19,5 @@ const newData = new APIdata();
 myWatchlist.getCollection(data => {
     newData.getFilm(data)
         .then(result => myList.render(result))
+        .catch(err => console.log(err))
 });
-
-
-
-// EventListeners (Hover/Click)
-
-// Watch Status Toggle - Hover Animation
-boxContent.onmouseenter = function () {
-    watchToggle.classList.remove('hidden')
-    watchToggle.classList.add('flex')
-    setTimeout(() => watchToggle.classList.remove('opacity-0'), 10);
-    boxImg.classList.add('transform');
-
-}
-boxContent.onmouseleave = function () {
-    watchToggle.classList.add('opacity-0');
-    setTimeout(() => {
-        watchToggle.classList.remove('flex');
-        watchToggle.classList.add('hidden');
-    }, 300)
-    boxImg.classList.remove('transform');
-}
-
-
-// Add more functionality to it - make sure it got add/remove from Database
-const Watchstatus = {
-    watched: "./css/icons/ic_eye-watched.svg",
-    didntWatch: "./css/icons/ic_eye-empty.svg",
-    state: false
-}
-watchToggle.addEventListener('click', () => {
-    if (Watchstatus.state) {
-        watchToggle.firstElementChild.src = Watchstatus.didntWatch;
-        Watchstatus.state = false
-    } else {
-        watchToggle.firstElementChild.src = Watchstatus.watched;
-        Watchstatus.state = true
-    }
-})
--- scripts/ui.js
+++ scripts/ui.js
@@ -8,7 +8,7 @@ class siteUI {
             <div class="global-transition relative w-full h-full bg-cover bg-no-repeat bg-center scale-110"
                 style="background-image: url(${film.poster_path});">
                 <div data-name="overlay"
-                    class="absolute inset-0 bg-gradient-to-tr from-overlayBlack bg-opacity-20 flex justify-start items-end">
+                    class="absolute inset-0 bg-gradient-to-tr from-overlayBlack bg-opacity-40 flex justify-start items-end">
                 </div>
             </div>
             <div class="absolute left-3 bottom-3">
@@ -23,6 +23,46 @@ class siteUI {
         `;
 
         this.list.innerHTML += html;
+
+        // EventListeners (Hover/Click)
+        for (let i = 0; i < boxContent.length; i++) {
+            // Watch Status Toggle - Hover Animation
+            boxContent[i].onmouseenter = function () {
+                const boxImg = boxContent[i].firstElementChild;
+                watchToggle[i].classList.remove('hidden')
+                watchToggle[i].classList.add('flex')
+                setTimeout(() => watchToggle[i].classList.remove('opacity-0'), 10);
+                boxImg.classList.add('transform');
+
+            }
+            boxContent[i].onmouseleave = function () {
+                const boxImg = boxContent[i].firstElementChild;
+                watchToggle[i].classList.add('opacity-0');
+                setTimeout(() => {
+                    watchToggle[i].classList.remove('flex');
+                    watchToggle[i].classList.add('hidden');
+                }, 300)
+                boxImg.classList.remove('transform');
+            }
+
+            // Add more functionality to it - make sure it got add/remove from Database
+            const Watchstatus = {
+                watched: "./css/icons/ic_eye-watched.svg",
+                didntWatch: "./css/icons/ic_eye-empty.svg",
+                state: false
+            }
+            watchToggle[i].onclick = function () {
+                if (Watchstatus.state) {
+                    watchToggle[i].firstElementChild.src = Watchstatus.didntWatch;
+                    Watchstatus.state = false
+                } else {
+                    watchToggle[i].firstElementChild.src = Watchstatus.watched;
+                    Watchstatus.state = true
+                }
+
+            }
+        }
+
     }
 }
 
